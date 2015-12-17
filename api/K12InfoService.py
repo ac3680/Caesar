7496552830d9b14e644cefc64ce8a97386772006
Nina Baculinao <nb2406@columbia.edu>
2015-12-17 11:57:53 -0500

    GET, POST, PUT, DEL ops for K-12 microservice.

---
+++ api/K12InfoService.py
@@ -0,0 +1,146 @@
+import requests
+from bson.json_util import dumps
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
+from flask import Flask, url_for
+from flask import request
+from flask import json
+from flask import Response
+from flask import jsonify
+
+# Set up Dynamo
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('students')
+
+# Set up Flask
+app = Flask(__name__)
+app.config['DYNAMO_TABLES'] = [table]
+
+#==============================================================================
+# Database Operations: (C)reate, (R)ead, (U)pdate, (D)elete
+#==============================================================================
+
+# Create - Capable of overwrite
+def create_item(student):
+    table.put_item(
+        Item=student
+    )
+    return student
+
+# Read
+def find_item(uid):
+    response = table.get_item(
+        Key={ 'uid': uid }
+    )
+    return response['Item'] if 'Item' in response else None
+
+def find_all_items():
+    response = table.scan()
+    return response['Items'] if 'Items' in response else []
+
+# Update - Reads first, then re-Creates
+def update_item(uid, new_student_data):
+    student = find_item(uid)
+    if student:
+        for (key, value) in new_student_data.iteritems():
+            student[key] = value
+        create_item(student)
+        return student
+    else:
+        return None
+
+# Delete
+def delete_item(uid):
+    table.delete_item(
+    Key={ 'uid': uid }
+    )
+
+#==============================================================================
+# REST Operations
+#==============================================================================
+
+# POST .../students - Create a new student
+@app.route('/students', methods = ['POST'])
+def post_student():
+    data = form_or_json()
+    student = {key: value for (key, value) in data.iteritems()}
+    if 'uid' not in student:
+        return "Need uid to create new student\n", 409
+    uid = student['uid']
+    if find_item(uid):
+        return "The student(" + uid + ") already exists", 409
+    create_item(student)
+    message = "New student(" + uid+ ") created\n"
+    return message, 201
+
+# GET .../students/<uid> - Get a student by uid
+@app.route('/students/<uid>', methods = ['GET'])
+def get_student(uid):
+    student = find_item(uid)
+    if student:
+        return dumps(student), 200
+    else:
+        return not_found()
+
+# GET .../students - Get all students
+@app.route('/students', methods = ['GET'])
+def get_all_students():
+    students = find_all_items()
+    if students:
+        return dumps(students), 200
+    else:
+        return not_found()
+
+# PUT .../students/<uid> - Update student field
+@app.route('/students/<uid>', methods=['PUT'])
+def update_student(uid):
+    data = form_or_json()
+    student = {key: value for (key, value) in data.iteritems()}
+    if 'uid' in student:
+        return "Updating a student's uid is forbidden\n", 409
+    new_student = update_item(uid, student)
+    if new_student:
+        return dumps(new_student), 203
+    else:
+        return not_found()
+
+# DELETE .../students/<uid> - Delete a student
+@app.route('/students/<uid>', methods=['DELETE'])
+def delete_student(uid):
+    student = find_item(uid)
+    if student:
+        delete_item(uid)
+        return "Student(" + uid + ")deleted successfully", 204
+    else:
+        return not_found()
+
+#==============================================================================
+# Error Handling
+#==============================================================================
+
+# Handle nonexistent routes
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+            'status': 404,
+            'message': 'Not Found: ' + request.url,
+    }
+    response = jsonify(message)
+    response.status_code = 404
+    return response
+
+#==============================================================================
+# Helper Methods
+#==============================================================================
+
+# Returns data whether from request.form or request.data
+def form_or_json():
+    data = request.data
+    return json.loads(data) if data is not '' else request.form
+
+
+if __name__ == '__main__':
+    app.run(
+        debug = True,
+        port = 9002
+    )
